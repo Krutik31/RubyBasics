478bfbbc36bcde37f5959200461503b812e12d9d
Krutik Patel <krutik.p@simformsolutions.com>
2023-02-17 16:01:09 +0530

    Added file-01

---
+++ 01_helloworld.rb
@@ -0,0 +1,13 @@
+# In this file, we are printing "Hello World" string using different ways of ruby programming. 
+
+# print keyword is used to print the string when we don't want to point a cursor to the next line. 
+print "Hello"
+print "World"
+print "\n"
+
+# puts is used when we want to print something and along with that, we also want to point the cursor to the next line.
+puts "Hello"
+puts "World"
+
+# p keyword is used for showing more raw version of an object.
+p "Hello World"
