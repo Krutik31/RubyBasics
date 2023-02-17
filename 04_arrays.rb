23a24ba9f94659e4d2fe290143d769f20a14fc14
Krutik Patel <krutik.p@simformsolutions.com>
2023-02-17 18:30:12 +0530

    Added file-04

---
+++ 04_arrays.rb
@@ -0,0 +1,45 @@
+users = ["Krutik", "Dhyey", nil, "Aditya"]
+
+print users
+print "\n"
+
+# get the size of the array
+print users.length
+print "\n"
+
+# Accessing an elements
+print users[0]
+print "\n"
+print users[0,4]
+print "\n"
+
+# at keyword
+print users.at(-1)
+print "\n"
+
+# converting string into array.
+str = "My name is Krutik Patel"
+print str.split
+print "\n"
+
+# first & last methods
+print users.first
+print "\n"
+print users.last
+print "\n"
+
+# Insert into array
+print users.push("Aman")
+print "\n"
+print users << "Nil"
+print "\n"
+print users.insert(2, "Shiva")
+print "\n"
+
+# Delete from array
+users.pop
+print users
+print "\n"
+users.delete(nil)
+print users
+print "\n"
