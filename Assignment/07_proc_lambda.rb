f7bea90cfa3dad5ef92e06890cdbdeee5079601c
Krutik Patel <krutik.p@simformsolutions.com>
2023-02-22 17:24:52 +0530

    Added seventh exercise

---
+++ Assignment/07_proc_lambda.rb
@@ -0,0 +1,29 @@
+def proc_lambda_demo
+
+    puts "before lambda"
+
+	lambdavar = lambda {
+		puts "inside lambda"
+		return            #It will return only from lambda proc, not from an entire method
+	}
+	lambdavar.call
+
+	puts "after lambda"
+
+	procvar = proc {
+		puts "inside proc"
+		return            #It will return from proc as well as from an entire method. that's why "after proc" statement will not be printed in final output.
+	}
+	procvar.call
+
+	puts "after proc"
+end
+
+proc_lambda_demo
+
+
+# Output:
+    # before lambda
+    # inside lambda
+    # after lambda
+    # inside proc
